d44d34c7c811f67922e36d34fa66aa9bf8298926
Tony Nguyen <tony@tonyfreelance.com>
2016-03-17 14:25:12 +0000

    Change callback setting

--- config/twitter.js
+++ config/twitter.js
@@ -1,6 +1,6 @@
 module.exports.twitter = {
     consumer_key: 'wxf1f5PnsLbGfidQ74zvHSS6C',
     consumer_secret: '7082Zh8NASUQdKfCe4AC5Y1gnU8HFj0fp2NqOoMd8yT5hUvQtv',
-    callback: 'https://sails-test-tonyfreelance-1.c9users.io/auth/twitter/callback',
+    callback: 'https://tony-voting-app.herokuapp.com/auth/twitter/callback',
     tokenSecret: 'tonyfreelance'
 };
