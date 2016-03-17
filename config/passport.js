46f1c79aba9967a9213c648d54c9101b329457ab
Tony Nguyen <tony@tonyfreelance.com>
2016-03-17 14:17:40 +0000

    Comment out bcrypt

--- config/passport.js
+++ config/passport.js
@@ -1,17 +1,17 @@
-var passport = require('passport'),
-    LocalStrategy = require('passport-local').Strategy,
-    TwitterStrategy = require("passport-twitter").Strategy,
-    bcrypt = require('bcrypt');
+// var passport = require('passport'),
+//     LocalStrategy = require('passport-local').Strategy,
+//     TwitterStrategy = require("passport-twitter").Strategy,
+//     bcrypt = require('bcrypt');
 
-passport.serializeUser(function(user, done) {
-    done(null, user.id);
-});
+// passport.serializeUser(function(user, done) {
+//     done(null, user.id);
+// });
 
-passport.deserializeUser(function(id, done) {
-    User.findOneById(id).exec(function(err, user) {
-        done(err, user);
-    });
-});
+// passport.deserializeUser(function(id, done) {
+//     User.findOneById(id).exec(function(err, user) {
+//         done(err, user);
+//     });
+// });
 
 // passport.use(new LocalStrategy({
 //         usernameField: 'email',
@@ -54,34 +54,34 @@ passport.deserializeUser(function(id, done) {
 // ));
 
 // For Twitter authentication
-passport.use(new TwitterStrategy({
-        consumerKey: 'wxf1f5PnsLbGfidQ74zvHSS6C',
-        consumerSecret: '7082Zh8NASUQdKfCe4AC5Y1gnU8HFj0fp2NqOoMd8yT5hUvQtv',
-        callbackURL: 'https://sails-test-tonyfreelance-1.c9users.io/auth/twitter/callback',
-    },
-    function(token, tokenSecret, profile, done) {
-        process.nextTick(function() {
-           User.findOne({
-               id: profile.id
-           }, function(err, existingUser) {
-               if(err) {
-                   return done(err);
-               }
-               if(existingUser) {
-                   return done(null, existingUser);
-               } else {
-                   var newUser = {
-                     id: profile.id,
-                     token: token,
-                     displayName: profile.displayName,
-                     username: profile.username
-                   };
+// passport.use(new TwitterStrategy({
+//         consumerKey: 'wxf1f5PnsLbGfidQ74zvHSS6C',
+//         consumerSecret: '7082Zh8NASUQdKfCe4AC5Y1gnU8HFj0fp2NqOoMd8yT5hUvQtv',
+//         callbackURL: 'https://sails-test-tonyfreelance-1.c9users.io/auth/twitter/callback',
+//     },
+//     function(token, tokenSecret, profile, done) {
+//         process.nextTick(function() {
+//           User.findOne({
+//               id: profile.id
+//           }, function(err, existingUser) {
+//               if(err) {
+//                   return done(err);
+//               }
+//               if(existingUser) {
+//                   return done(null, existingUser);
+//               } else {
+//                   var newUser = {
+//                      id: profile.id,
+//                      token: token,
+//                      displayName: profile.displayName,
+//                      username: profile.username
+//                   };
                    
-                   User.create(newUser, function(err, user) {
-                      if(err) sails.log(err);
-                      return done(null, user);
-                   });
-               }
-           }); 
-        });
-    }));
+//                   User.create(newUser, function(err, user) {
+//                       if(err) sails.log(err);
+//                       return done(null, user);
+//                   });
+//               }
+//           }); 
+//         });
+//     }));
